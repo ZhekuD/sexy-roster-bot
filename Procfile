f532eb92fa1b3eb3336ae7617ceca2cb60aba111
Eugene <zhekud@gmail.com>
2020-01-11 15:18:38 +0200

    configurate app to deploy on Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python sexyrosterbot/main.py
+web: gunicorn -b 0.0.0.0:$PORT main:server
--- sexyrosterbot/main.py
+++ sexyrosterbot/main.py
@@ -1,23 +1,28 @@
-import telebot
-import os
 from flask import Flask, request
 from configparser import ConfigParser
+from io import StringIO
+from requests import get
+import telebot
+import os
+
+from roster_parser import roster_parser
 
 
 config = ConfigParser()
 config.read('config.ini')
 
-# TOKEN = config['bot']['Token']
-TOKEN = "TOKEN"
-BOT_URL = "https://boiling-spire-19861.herokuapp.com/bot"
+TOKEN = os.environ.get('TOKEN') or config['bot']['Token']
 PORT = os.environ.get('PORT', 80)
+BOT_URL = "https://boiling-spire-19861.herokuapp.com/bot"
 
 server = Flask(__name__)
 bot = telebot.TeleBot(TOKEN)
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))]
+    )
     return "!", 200
 
 @server.route("/")
@@ -28,11 +33,22 @@ def webhook():
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello there!")
+    bot.reply_to(
+        message,
+        "Hello there! Send me your BattleScibe roster in HTML format \
+        and I will make it pretty-looking and easy readable on your devices!"
+    )
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(content_types=['document'])
+def send_document(message):
+    file_info = bot.get_file(file_id=message.document.file_id)
+    url = f'https://api.telegram.org/file/bot{TOKEN}/{file_info.file_path}'
+    resp = get(url)
+
+    file = StringIO(roster_parser(resp.text))
+    file.name = 'edited_' + message.document.file_name
+    bot.send_document(message.chat.id, file)
+    file.close()
 
 if os.environ.get('HEROKU'):
     server.run(host="0.0.0.0", port=PORT)
